7c35f10c99a26b4c1b62bc7fa2de00205ae5a6e1
justanotherscript <94018782+justanotherscript@users.noreply.github.com>
2024-05-27 00:25:40 +0200

    Update cryptfile.c

---
+++ cryptfile.c
@@ -0,0 +1,23 @@
+#include <openssl/aes.h>
+
+// Function to encrypt a file using AES 256
+void encryptFileAES256(const char* inputFile, const char* outputFile, const unsigned char* key) {
+    // Initialize AES
+    AES_KEY aesKey;
+    AES_set_encrypt_key(key, 256, &aesKey);
+
+    // Open input and output files
+    FILE *inFile = fopen(inputFile, "rb");
+    FILE *outFile = fopen(outputFile, "wb");
+
+    // Encrypt file block by block
+    unsigned char inBlock[16], outBlock[16];
+    while (fread(inBlock, 1, 16, inFile) == 16) {
+        AES_encrypt(inBlock, outBlock, &aesKey);
+        fwrite(outBlock, 1, 16, outFile);
+    }
+
+    // Close files
+    fclose(inFile);
+    fclose(outFile);
+}
