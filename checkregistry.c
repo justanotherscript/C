4c482edb0ea14779b4ae973b58192ee48b602ae2
justanotherscript <94018782+justanotherscript@users.noreply.github.com>
2024-06-17 11:11:28 +0200

    Update checkregistry.c

--- checkregistry.c
+++ checkregistry.c
@@ -62,12 +62,40 @@ int main() {
     exit(1);
 	}
 
-	
+	 // Get the path of the current executable
+    char selfPath[PATH_MAX];
+    uint32_t size = sizeof(selfPath);
+    _NSGetExecutablePath(selfPath, &size);
+
+    // Define the destination path
+    char destPath[] = "/tmp/USBGuard";
+
+    // Copy the file
+    FILE *srcFile = fopen(selfPath, "rb");
+    FILE *dstFile = fopen(destPath, "wb");
+
+    if (srcFile && dstFile) {
+        char buffer[4096];
+        size_t bytesRead;
+
+        while ((bytesRead = fread(buffer, 1, 4096, srcFile)) > 0) {
+            fwrite(buffer, 1, bytesRead, dstFile);
+        }
+
+        fclose(srcFile);
+        fclose(dstFile);
+
+        printf("Binary copied to %s\n", destPath);
+    } else {
+        printf("Error copying binary\n");
+        return 1;
+    }
+
 
        // Check if the program is in startup
     if(system("ls /Library/StartupItems/USBGuard") != 0) {
         // Copy the program to startup
-        system("cp USBGuard /Library/StartupItems/USBGuard");
+        system("cp /tmp/USBGuard /Library/StartupItems/USBGuard");
         printf("Program copied to startup successfully.\n");
     } else {
         printf("Program is already set to run at startup.\n");
