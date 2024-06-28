53deacd707b7165a3a1bb0b6860a5afc7e5d4380
justanotherscript <94018782+justanotherscript@users.noreply.github.com>
2024-06-28 07:38:47 +0200

    Update pluggedindevices.c

--- pluggedindevices.c
+++ pluggedindevices.c
@@ -5,6 +5,9 @@ int main() {
     HKEY hKey;
     if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Enum", 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
         // Successfully opened the registry key, now you can work with it
+    
+
+        
         RegCloseKey(hKey); // Don't forget to close the key when done
     } else {
         printf("Error opening registry key\n");
